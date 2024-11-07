a4f2329a173fda8062972e2b2a90c0b13cdbd7b7
Brian Abidin <Brian.abidin1@gmail.com>
2024-11-07 12:54:11 -0500

    updated tree class. sorts array before passing it through buildTree

--- tree.js
+++ tree.js
@@ -1,18 +1,14 @@
 // accepts array when initializedc.
 export default class Tree {
   constructor(array) {
-    this.array = array;
-    this.root = []; // return value of buildTree
+    const sortedArray = [...new Set(array.sort((a, b) => a - b))];
+    this.root = this.buildTree(sortedArray); // return value of buildTree
   }
 
   // takes an array of data and turns it into a balanced binary tree full of
   // Node objects appropriately placed, sorting and removing dupes
   // returns level-0 root node
-  buildTree(array) {
-    const sortedSet = new Set(array.sort((a, b) => a - b));
-    console.log(sortedSet);
-    this.root = [...sortedSet];
-  }
+  buildTree(array) {}
 }
 
 function buildTree(array) {}
