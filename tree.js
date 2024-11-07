e7c793570c6bf0d74f65d53885f8736319668e8c
Brian Abidin <Brian.abidin1@gmail.com>
2024-11-07 13:31:42 -0500

    updated buildtree method

--- tree.js
+++ tree.js
@@ -1,4 +1,5 @@
-// accepts array when initializedc.
+import Node from "./node.js";
+// accepts array when initialized.
 export default class Tree {
   constructor(array) {
     const sortedArray = [...new Set(array.sort((a, b) => a - b))];
@@ -8,7 +9,43 @@ export default class Tree {
   // takes an array of data and turns it into a balanced binary tree full of
   // Node objects appropriately placed, sorting and removing dupes
   // returns level-0 root node
-  buildTree(array) {}
+  buildTree(array) {
+    console.log(array, "Array");
+    if (array.length === 0) return null;
+    const mid = Math.floor(array.length / 2);
+    const node = new Node(array[mid]);
+
+    const leftArr = array.slice(0, mid);
+    const rightArr = array.slice(mid + 1, array.length);
+
+    node.left = this.buildTree(leftArr);
+    node.right = this.buildTree(rightArr);
+
+    return node;
+  }
+
+  // [1, 2, 3, 4, 5]
+  // false
+  // node {data: 3, left, right}
+  // leftArr = [1, 2]
+  // rightArr = [3, 4]
+  // line 21: recursion starts
+  // > array = [1, 2]
+  // false
+  // node {data: 2, left, right}
+  // leftArr = [1]
+  // rightArr = []
+  // line 21: recursion again
+  // > array = [1]
+  // false
+  // node {data: 1, left, right}
+  // leftArr = []
+  // rightArr = []
+  // line 21: recursion again
+  // > array []
+  // true return null
+  // node {data: 1, left = null, right = null}
+  //
 }
 
 function buildTree(array) {}
